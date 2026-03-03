824d189853d801532e3222bd0ab3fd570d424cbf
Yu-Jaeyoung <jackyu0721@gmail.com>
2026-03-03 23:46:18 +0900

    set streamlit, build default chat UI

---
+++ challenge/4/main.py
@@ -0,0 +1,70 @@
+# source .venv/bin/activate
+# streamlit run main.py
+# deactivate
+
+import dotenv
+import asyncio
+import streamlit as st
+from agents import Agent, Runner, SQLiteSession
+
+dotenv.load_dotenv()
+
+instruction = """
+1. Core Persona and Tone
+Role: Act as a dedicated, encouraging, and professional life coach.
+Attitude: Maintain a supportive, empathetic, and positive demeanor. Always validate the user's feelings and acknowledge their efforts without judgment.
+Communication Style: Use uplifting language that inspires confidence and motivates the user to achieve their goals.
+
+2. Tool Utilization (Web Search)
+Active Searching: Proactively use the web search tool to find highly relevant, accurate, and practical advice tailored to the user's specific challenges.
+Evidence-Based Guidance: Ground your coaching in credible sources, expert advice, and proven self-improvement methodologies retrieved through your search capabilities.
+
+3. Coaching Strategy
+Actionable Steps: Translate abstract advice into small, manageable, and concrete steps that the user can immediately implement.
+Empathetic Engagement: Briefly summarize the user's situation to demonstrate understanding and empathy before proposing solutions.
+Continuous Encouragement: Always conclude your responses with a reinforcing and motivational statement to build the user's momentum.
+"""
+
+if "agent" not in st.session_state:
+    st.session_state["agent"] = Agent(
+        name="Life Coach Agent",
+        instructions=instruction
+    )
+
+agent = st.session_state["agent"]
+
+if "session" not in st.session_state:
+    st.session_state["session"] = SQLiteSession(
+        "chat-history"
+        , "chat-gpt-clone-memory-db"
+    )
+
+session = st.session_state["session"]
+
+async def run_agent(message):
+    stream = Runner.run_streamed(
+        agent,
+        message,
+        session=session
+    )
+
+    async for event in stream.stream_events():
+        if event.type == "raw_response_event":
+            if event.data.type == "response.output_text.delta":
+                with st.chat_message("ai"):
+                    st.write(event.data.delta)
+
+
+prompt = st.chat_input("Write a message for your assistant")
+
+if prompt:
+    with st.chat_message("human"):
+        st.write(prompt)
+
+    asyncio.run(run_agent(prompt))
+
+with st.sidebar:
+    reset = st.button("Reset memory")
+    if reset:
+        asyncio.run(session.clear_session())
+    st.write(asyncio.run(session.get_items()))
